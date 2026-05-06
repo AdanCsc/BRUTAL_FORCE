7785090d69ff5b2836dd71d2b44b36aaeb172123
unknown <adanchristiam05@gmail.com>
2026-05-06 20:54:25 -0300

    BRUTAL_FORCE

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const url = "http://localhost:3005"
+
+async function bruteForceSimples() {
+  let tentativas = 0;
+
+  while (tentativas < 1000) {
+    try {
+      const res = await axios.get(url);
+      console.log(`Tentativa ${tentativas}:`, res.data);
+    } catch (err) {
+      console.log(`Erro na tentativa ${tentativas}`);
+    }
+
+    tentativas++;
+  }
+}
+
+bruteForceSimples();
